1b1ce80fbc8f108303d46e6ab8b10708ac2ed45a
Abul Hasnat Nayeem <nayeem2693@gmail.com>
2019-10-31 13:53:51 +0100

    Update example config

--- weather/config.example.py
+++ weather/config.example.py
@@ -1,4 +1,5 @@
 API = {
-    'base_url': 'http://api.openweathermap.org/data/2.5'
-    'key': 'API_KEY',
+    'base_url': 'http://api.openweathermap.org/data/2.5',
+    'key': 'api_key',
+    'captial_ids': '3435910,3903987,3469058,3871336,3688689,3652462,3378644,3439389,3936456,3383330,3441575,3646738',
 }
