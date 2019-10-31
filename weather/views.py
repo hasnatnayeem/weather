ccbecc22e847dc31e525a932f293746a286b2a34
Abul Hasnat Nayeem <nayeem2693@gmail.com>
2019-10-31 13:11:58 +0100

    Add example setting file

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import json
+from . import config
+
 
 def current_weather(request):
-    return HttpResponse("works")
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=de3eb0b64c75657b96f68709f813d718'
+    city = request.ipinfo.city
+    
+    return HttpResponse(json.dumps(request.ipinfo))
